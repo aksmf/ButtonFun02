99157021f6ce1914c07341f503cbf32909ac9b74
D7703_28 <d7703_28@d7703-28ui-imac.local>
2019-03-21 16:50:39 +0900

    commit 전송완료

--- buttonfun/ViewController.swift
+++ buttonfun/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var labelB: UILabel!
+    @IBOutlet var mylabel: UILabel!
+    @IBOutlet var contectlabel: UILabel!
+    
+    var count=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        contectlabel.text=String(count)
     }
 
 
+    @IBAction func button(_ sender: Any) {
+        mylabel.text="공민혁"
+        count+=1
+        contectlabel.text=String(count)
+    }
+    
+    
+    @IBAction func button2(_ sender: Any) {
+        labelB.text="123123"
+    }
+    
+    
+    
 }
+
+
+
 
